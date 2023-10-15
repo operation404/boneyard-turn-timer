f4c882ac61bb345301bb30733e5a204017f6d323
operation404 <operation404@gmail.com>
2023-10-14 18:21:34 -0700

    Quick helper function to change styles on elements easily

---
+++ scripts/helpers.js
@@ -0,0 +1,16 @@
+/**
+ * Find the elements in html described by the keys of elementsToStyle, the corresponding
+ * value of which is an object describing each style to update with its keys and the value
+ * to update them with as the corresponding values.
+ * @param {HTMLElement} html
+ * @param {Object} elementsToStyle
+ */
+export function applyCustomStyling(html, elementsToStyle = {}) {
+    if (html.querySelector && typeof elementsToStyle === 'object')
+        for (const [elementQuery, elementStyles] of Object.entries(elementsToStyle)) {
+            const element = html.querySelector(elementQuery);
+            if (element && typeof elementStyles === 'object')
+                for (const [styleId, styleVal] of Object.entries(elementStyles))
+                    if (typeof styleVal === 'string') elementQuery[styleId] &&= styleVal;
+        }
+}
