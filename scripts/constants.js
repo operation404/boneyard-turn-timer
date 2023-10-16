c287e1752a37dcc0657ac8bad6c9c6437e0ed8c8
operation404 <operation404@gmail.com>
2023-10-16 16:39:26 -0700

    Simplify constants

--- scripts/constants.js
+++ scripts/constants.js
@@ -3,24 +3,26 @@ export const SOCKET = 'module.boneyard-turn-timer';
 export const TIMER_TEMPLATE_PATH = 'modules/boneyard-turn-timer/templates/timer-bar.hbs';
 export const CONTROL_TEMPLATE_PATH = 'modules/boneyard-turn-timer/templates/toggle-control.hbs';
 
-const settings = [
-    'ACTIVE',
-    'FORCE_TURN_CHANGE',
-    'DEFAULT_TURN_DURATION',
-    'CUSTOM_TURN_DURATIONS',
-    'BAR_COLOR',
-    'WARNING_THRESHOLD',
-    'WARNING_COLOR',
-    'WARNING_SOUND',
-    'TURN_START_SOUND',
-    'NEXT_UP_ALERT',
-    'NEXT_UP_SOUND',
-    'AUTO_POPOUT',
-    'POPOUT_POSITION',
-    'POPOUT_WIDTH',
-];
-export const SETTINGS = Object.freeze(Object.fromEntries(settings.map((v) => [v, v])));
-
+export const SETTINGS = Object.freeze(
+    Object.fromEntries(
+        [
+            'ACTIVE',
+            'FORCE_TURN_CHANGE',
+            'DEFAULT_TURN_DURATION',
+            'CUSTOM_TURN_DURATIONS',
+            'BAR_COLOR',
+            'WARNING_THRESHOLD',
+            'WARNING_COLOR',
+            'WARNING_SOUND',
+            'TURN_START_SOUND',
+            'NEXT_UP_ALERT',
+            'NEXT_UP_SOUND',
+            'AUTO_POPOUT',
+            'POPOUT_POSITION',
+            'POPOUT_WIDTH',
+        ].map((v) => [v, v])
+    )
+);
 
 export const INTERVAL_STEP = 25; // milliseconds
 export const MINIMUM_TURN_DURATION = 1; // seconds
